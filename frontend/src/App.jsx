adbed91020436c45a532b4a14bdae8dd242d7d15
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-07-12 11:18:16 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Dashboard from './pages/Dashboard';
+// import Browse from './pages/Browse';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      {/* <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/browse" element={<Browse />} /> */}
+    </Routes>
   )
 }
 
