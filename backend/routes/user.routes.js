69f1fac634b7117c76ee79e2c6732a9cb1f34cfe
anjaanid <sujj4646@gmail.com>
2025-07-12 13:48:36 +0530

    created register user, login user, logout user, and added config for cors

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,14 @@
+import {Router} from 'express';
+import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
+import { verifyJWT } from '../middleware/auth.middleware.js';
+
+const router = Router();
+
+// unsecured routes
+router.route('/signup').post(registerUser);
+router.route("/login").post(loginUser);
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+
+export default router;
