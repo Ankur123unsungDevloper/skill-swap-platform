aa61efa55a595cdd126157415193706d618fc816
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-07-23 20:29:40 +0530

    modified:   frontend/src/App.tsx 	modified:   frontend/src/main.tsx 	deleted:    frontend/src/routes/routes.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './main/landing-page/landing-page';
 import DashBoard from './dashboard/dashboard';
-import SignInPage from './routes/sign-in';
-import SignUpPage from './routes/sign-up';
 
 const App= () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/sign-in/*" element={<SignInPage />} />
-      <Route path="/sign-up/*" element={<SignUpPage />} />
       <Route path="/dashboard" element={<DashBoard />} />
     </Routes>
   );
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import { ClerkProvider } from '@clerk/react-router'
-import './index.css'
-import App from './App.tsx'
-
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </ClerkProvider>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
